93c644ae54d992fb50e9956a1d155e1cf615d48c
Swaraj Gupta <contact.swaraj@gmail.com>
2022-07-04 03:35:39 +0530

    Revert "Initial Commit for  Transfer Api"
    This reverts commit fc3c53d135d24d7f72dc00a6ed95b84ae68b0e70.

--- transfer-api/account-service/src/main/java/org/account/api/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package org.account.api.repository;
-
-import org.account.api.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account,String> {
-}
--- transfer-api/event-service/src/main/java/org/event/api/repository/TransferEventRepository.java
+++
@@ -1,9 +0,0 @@
-package org.event.api.repository;
-
-import org.event.api.entity.TransferEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransferEventRepository extends JpaRepository<TransferEvent,String> {
-}
